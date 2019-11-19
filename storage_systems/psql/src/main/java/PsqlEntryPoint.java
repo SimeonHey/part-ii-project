436b9691fcd72bb0b25a030c05b03b0108862d7e
Simeon Stoykov <ss2476@cam.ac.uk>
2019-11-19 16:38:45 +0000

    PSQL backend implemented

--- storage_systems/psql/src/main/java/PsqlEntryPoint.java
+++ storage_systems/psql/src/main/java/PsqlEntryPoint.java
@@ -3,17 +3,29 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class PsqlEntryPoint {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         // Consume program line arguments
         int argListeningPort = Integer.parseInt(args[0]);
+        String argPsqlAddress = args[1];
+        String[] argUserPass = args[2].split(":");
 
-        // Connect to Kafka & possibly the storage api
+        // Connect to Kafka & PSQL(
         LoopingConsumer<Long, StupidStreamObject> consumer =
             new LoopingConsumer<>(KafkaUtils.createConsumer("psql"));
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(argListeningPort), 0);
-        PsqlWrapper psqlWrapper = new PsqlWrapper();
+
+        Properties props = new Properties();
+        props.setProperty("user", argUserPass[0]);
+        props.setProperty("password", argUserPass[1]);
+        Connection conn = DriverManager.getConnection(argPsqlAddress, props);
+        PsqlWrapper psqlWrapper = new PsqlWrapper(conn);
+
         Gson gson = new Gson();
 
         PsqlStorageSystem luceneStorageSystem = new PsqlStorageSystem(consumer, httpServer, psqlWrapper, gson);
--- storage_systems/psql/src/main/java/PsqlWrapper.java
+++ storage_systems/psql/src/main/java/PsqlWrapper.java
@@ -1,10 +1,68 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PsqlWrapper {
+    private Connection connection;
+
+    public PsqlWrapper(Connection connection) {
+        this.connection = connection;
+    }
+
+    private void insertMessage(String sender, String messageText, Long uuid) throws SQLException {
+        connection.setAutoCommit(false);
+
+        String query = String.format("INSERT INTO messages (sender, messageText, uuid) VALUES ('%s', '%s', %d)",
+            sender,
+            messageText,
+            uuid);
+        System.out.println(query);
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.executeUpdate();
+        connection.commit();
+    }
+
     public void postMessage(PostMessageRequest postMessageRequest, Long uuid) {
         System.out.println("PSQL posts message " + postMessageRequest + " with uuid " + uuid);
+        try {
+            insertMessage(postMessageRequest.getSender(), postMessageRequest.getMessageText(), uuid);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getMessageDetails(MessageDetailsRequest messageDetailsRequest) {
-        return "Psql has to get details for message " + messageDetailsRequest.getUuid() + " but too lazy.";
+        System.out.println("Psql has to get details for message " + messageDetailsRequest.getUuid());
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(String.format("SELECT * FROM messages WHERE uuid " +
+                "= %d", messageDetailsRequest.getUuid()));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            StringBuilder sbBig = new StringBuilder();
+
+            int columnCount = resultSet.getMetaData().getColumnCount();
+
+            // TODO: Ugly
+            while (resultSet.next()) {
+                StringBuilder sbSmall = new StringBuilder();
+                for (int i=1; i<=columnCount; i++) {
+                    if (i < columnCount) {
+                        sbSmall.append(resultSet.getString(i)).append(" ");
+                    } else {
+                        sbSmall.append(resultSet.getLong(i)).append(" ");
+                    }
+                }
+                sbBig.append(sbSmall.toString()).append(" ");
+            }
+            preparedStatement.close();
+
+            return sbBig.toString();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 // TODO: Junit
